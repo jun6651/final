217ef896f94ce46bbf405b025a5c35ced79380ff
jun6651 <julia810400@gmail.com>
2023-12-22 16:44:03 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, make_response, jsonify
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x ="作者-林映均12/17<br>"
+    x +="<a href=/spider2>資管導論</a><br>"
+
+    return x
+
+@app.route("/spider2")
+def spider2():
+    info = ""
+
+    url = "https://www.xbanxia.com/list/10_1.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".pop-book2")
+
+    for x in result:
+            info += "<a href=" + x.find("a").get("href") + ">" + x.find("h2").text + "</a><br>"
+            info += x.find("a").get("href") + "<br>"
+            info += "<img src=https://www.xbanxia.com/" + x.find("img").get("src") + " width=200 height=300 " + "</img><br>"
+    return info
+
+@app.route("/book")
+def book():
+    url = "https://www.xbanxia.com/list/10_1.html"  
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".pop-book2")  #是否正確
+    info = ""
+    for item in result:
+      picture = item.find("img").get("src").replace(" ", "")
+      title = item.find("h2").text
+      book_id = item.find("a").get("href").replace("/", "").replace("book", "")
+      hyperlink = "https://www.xbanxia.com/list/10_1.html" + item.find("a").get("href")
+      info += picture + "\n" + title +"\n"+ hyperlink +"\n" 
+      doc = {
+          "title": title,
+          "picture": picture,
+          "hyperlink": hyperlink,
+      }
+
+      db = firestore.client()
+      doc_ref = db.collection("小說").document(book_id)
+      doc_ref.set(doc)
+    return "近期上映小說已爬蟲及存檔完畢"
+               
+
+@app.route("/webhook4", methods=["POST"])
+def webhook4():
+    req = request.get_json(force=True)
+    action =  req["queryResult"]["action"]
+    if (action == "BookDetail"): #book intent action&parameters
+        keyword =  req.get("queryResult").get("parameters").get("any") #book intent parameter name
+        info = "我是小說聊天機器人，您要查詢關鍵字是" + keyword + "的小說\n\n"
+        
+        
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

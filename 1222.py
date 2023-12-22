23a47821c648464701b6457d6a8b4006b2edfeb2
jun6651 <julia810400@gmail.com>
2023-12-22 18:38:13 +0800

    make it better

--- 1222.py
+++ 1222.py
@@ -1,15 +1,15 @@
 
-            db = firestore.client()
-            collection_ref = db.collection("小說")
-            docs = collection_ref.get()
-            Found = False
-            info = ""
-            for doc in docs:                                                                                                                
-                dict = doc.to_dict()
-                if keyword in dict["keyword"]: #原為if keyword in dict["title"]:
-                    found = True 
-                    info += "書名：" + dict["title"] + "\n"
-                    info += "封面：" + dict["picture"] + "\n"
-                    info += "書籍連結：" + dict["hyperlink"] + "\n"
+        db = firestore.client()
+        collection_ref = db.collection("小說")
+        docs = collection_ref.get()
+        Found = False
+        info = ""
+        for doc in docs:                                                                                                                
+            dict = doc.to_dict()
+            if keyword in dict["keyword"]:
+                found = True 
+                info += "書名：" + dict["title"] + "\n"
+                info += "封面：" + dict["picture"] + "\n"
+                info += "書籍連結：" + dict["hyperlink"] + "\n"
             if not found:
-                info += "很抱歉，目前無符合這個關鍵字的相關電影喔"
+                info += "很抱歉，目前無符合這個關鍵字的相關小說喔"

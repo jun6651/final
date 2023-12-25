9fef2b66b128d7312607d5c2e5abfcf5d42270e4
jun6651 <julia810400@gmail.com>
2023-12-25 10:10:17 +0800

    make it better

--- 1222.py
+++ 1222.py
@@ -1,15 +1,19 @@
 
+
+
         db = firestore.client()
         collection_ref = db.collection("小說")
         docs = collection_ref.get()
-        Found = False
+        found = False
         info = ""
         for doc in docs:                                                                                                                
-            dict = doc.to_dict()
-            if keyword in dict["keyword"]:
+            book_dict = doc.to_dict()
+            if keyword in book_dict["keyword"]:
                 found = True 
-                info += "書名：" + dict["title"] + "\n"
-                info += "封面：" + dict["picture"] + "\n"
-                info += "書籍連結：" + dict["hyperlink"] + "\n"
-            if not found:
-                info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
+                info += "書名：" + book_dict.get("title", "N/A") + "\n"
+                info += "封面：" + book_dict.get("picture", "N/A") + "\n"
+                info += "書籍連結：" + book_dict.get("hyperlink", "N/A") + "\n"
+
+        if not found:
+            info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
+
--- index.py
+++ index.py
@@ -68,13 +68,14 @@ def webhook4():
         docs = collection_ref.get()
         found = False
         info = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if keyword in dict["keyword"]:
-                found = True
-                info += "書名：" + dict["title"] + "\n"
-                info += "封面：" + dict["picture"] + "\n"
-                info += "書籍連結：" + dict["hyperlink"] + "\n"
+        for doc in docs:                                                                                                                
+            book_dict = doc.to_dict()
+            if keyword in book_dict["keyword"]:
+                found = True 
+                info += "書名：" + book_dict.get("title", "N/A") + "\n"
+                info += "封面：" + book_dict.get("picture", "N/A") + "\n"
+                info += "書籍連結：" + book_dict.get("hyperlink", "N/A") + "\n"
+
         if not found:
             info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
 

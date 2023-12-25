487c26115068715645440eb863be70ccb3fe59af
jun6651 <julia810400@gmail.com>
2023-12-25 15:26:40 +0800

    make it better

--- 1222.py
+++ 1222.py
@@ -1,19 +1,24 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
+db = firestore.client()
+collection_ref = db.collection("小說")
+docs = collection_ref.get()
+found = False
+info = ""
+for doc in docs:                                                                                                                
+    book_dict = doc.to_dict()
+    keyword = "的的"
+    if keyword in book_dict["title"]:
+        found = True 
+        info += "書名：" + book_dict.get("title", "N/A") + "\n"
+        info += "封面：" + book_dict.get("picture", "N/A") + "\n"
+        info += "書籍連結：" + book_dict.get("hyperlink", "N/A") + "\n"
 
-        db = firestore.client()
-        collection_ref = db.collection("小說")
-        docs = collection_ref.get()
-        found = False
-        info = ""
-        for doc in docs:                                                                                                                
-            book_dict = doc.to_dict()
-            if keyword in book_dict["keyword"]:
-                found = True 
-                info += "書名：" + book_dict.get("title", "N/A") + "\n"
-                info += "封面：" + book_dict.get("picture", "N/A") + "\n"
-                info += "書籍連結：" + book_dict.get("hyperlink", "N/A") + "\n"
-
-        if not found:
-            info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
+if not found:
+    info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
+print(info)
 
--- index.py
+++ index.py
@@ -3,6 +3,11 @@ from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
@@ -62,6 +67,21 @@ def webhook4():
     if action == "BookDetail":  # book intent action&parameters
         keyword = req.get("queryResult").get("parameters").get("any")
         info = "我是小說聊天機器人，您要查詢關鍵字是" + keyword + "的小說\n\n"
+        db = firestore.client()
+        collection_ref = db.collection("小說")
+        docs = collection_ref.get()
+        found = False
+        info = ""
+        for doc in docs:                                                                                                                
+            book_dict = doc.to_dict()
+            if keyword in book_dict["title"]:
+                found = True 
+                info += "書名：" + book_dict.get("title", "N/A") + "\n"
+                info += "封面：" + book_dict.get("picture", "N/A") + "\n"
+                info += "書籍連結：" + book_dict.get("hyperlink", "N/A") + "\n"
+
+        if not found:
+            info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
 
     return make_response(jsonify({"fulfillmentText": info}))
    

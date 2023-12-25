3660ba6188a0094818718fe8e27adb02484cf1a5
jun6651 <julia810400@gmail.com>
2023-12-25 09:52:23 +0800

    make it better

--- index.py
+++ index.py
@@ -75,8 +75,8 @@ def webhook4():
                 info += "書名：" + dict["title"] + "\n"
                 info += "封面：" + dict["picture"] + "\n"
                 info += "書籍連結：" + dict["hyperlink"] + "\n"
-            if not found:
-                info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
+        if not found:
+            info += "很抱歉，目前無符合這個關鍵字的相關小說喔"
 
     return make_response(jsonify({"fulfillmentText": info}))
 
